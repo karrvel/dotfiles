7e96639d1f04e8c04e435c8fa240a3cc0aeb98e6
karrvel <qosimovx20@gmail.com>
2023-06-02 23:55:08 +0500

    make executable; pretty echo

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 
 # Identify Package Manager
-echo ">Identifying Package Manager..."
+echo "> Identifying Package Manager..."
 
 declare -A osInfo;
 osInfo[/etc/redhat-release]="yum install"
@@ -12,10 +12,10 @@ osInfo[/etc/debian_version]="apt-get install"
 for f in ${!osInfo[@]}
 do
   if [[ -f $f ]];then
-    echo ">Package Manager found: " ${osInfo[$f]}
+    echo "> Package Manager found: " ${osInfo[$f]}
 
     # Install required packages
-    echo ">Installing required packages..."
+    echo "> Installing required packages..."
 
     sudo ${osInfo[$f]} wget git i3 i3blocks i3status feh flameshot kitty vim neovim picom zsh
   fi
@@ -23,7 +23,7 @@ done
 
 
 # Install Oh-My-Zsh and plugins for it
-echo ">Installing Oh-My-Zsh and plugins for it..."
+echo "> Installing Oh-My-Zsh and plugins for it..."
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -31,7 +31,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 
 # Get Dotfiles
-echo ">Getting Dotfiles... (will be saved at $HOME/dotfiles)"
+echo "> Getting Dotfiles... (will be saved at $HOME/dotfiles)"
 
 git clone https://github.com/karrvel/dotfiles $HOME/dotfiles
 
@@ -41,7 +41,7 @@ rm $HOME/.zshrc
 
 
 # Linking Proccess
-echo ">Linking Config Files..."
+echo "> Linking Config Files..."
 
 linker ()
 {
@@ -67,12 +67,12 @@ ln -s $HOME/dotfiles/fonts $HOME/.local/share/fonts
 
 
 # Update fonts
-echo ">Updating fonts..."
+echo "> Updating fonts..."
 
 $HOME/dotfiles/fonts/font-updater
 
 # Make zshctf file (See: https://github.com/karrvel/dotfiles/blob/main/zsh/.zshrc#L88)
 touch $HOME/.zshctf
 
-echo ">In order to change wallpaper check out i3 config (Line 20)"
-echo ">Installing Finished! Happy ricing)"
+echo "> In order to change wallpaper check out i3 config (:Line 20)"
+echo "> Installing Finished! Happy ricing)"
